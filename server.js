387da51effedf715c0846f1e5c0c3c5154057a77
Joshua Chuah <113268938+mnkhee@users.noreply.github.com>
2023-04-30 20:30:17 -0700

    update server to read from atlas

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = require("./app.js");
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/comp2537w1");
+    // await mongoose.connect("mongodb://127.0.0.1:27017/comp2537w1");
+    await mongoose.connect("mongodb+srv://mnkhee:jO3H6dS48jQjOn1j@cluster0.y8duirz.mongodb.net/comp2537w1?retryWrites=true&w=majority");
 
     //use `"await mongoose.connect("mongodb://user:password@127.0.0.1:17017/test");` if your database has auth enabled
     console.log("connected to db");
