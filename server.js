3048c78c50c2aa4a036f8d6cd2cad952123212cb
Joshua Chuah <113268938+mnkhee@users.noreply.github.com>
2023-04-30 19:59:57 -0700

    update server.js

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/comp2537w1");
 
     //use `"await mongoose.connect("mongodb://user:password@127.0.0.1:17017/test");` if your database has auth enabled
+    console.log("connected to db");
+    app.listen(3000, () => {
+        console.log("server is running on port 3000");
+    })
 }
